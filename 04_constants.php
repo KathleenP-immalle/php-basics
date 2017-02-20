9d6debb593f8ac4649017e072deed5e756e4fe46
kathleenP-immall <kathleen.puvrez@immalle.be>
2017-02-20 09:44:23 +0100

    oefening 4 af

--- 04_constants.php
+++ 04_constants.php
@@ -3,7 +3,15 @@
 define("USERNAME", "joske");
 
 echo USERNAME;
+echo "<br>";
 
 // definieer ook een constante PASSWORD en SERVERNAME
+
+define("PASSWORD", "password");
+echo PASSWORD;
+echo "<br>";
+
+define("SERVERNAME", "de server");
+echo SERVERNAME;
 
 ?>
