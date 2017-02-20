8aa8d695a52c8446c0c245e980956706ac0224d7
kathleenP-immall <kathleen.puvrez@immalle.be>
2017-02-20 10:03:08 +0100

    oefenin 6 bezig

--- 06_arrays.php
+++ 06_arrays.php
@@ -2,9 +2,13 @@
 
 // Maak een Array met 3 namen van medeleerlingen.
 
+$namearray = ["Thom", "Bert", "Liam"];
 
 // Itereer er over met een for-loop. Print ze af in een HTML ongenummerde lijst.
 
+for ($x = 0; $x < $namearray.length; $x++){
+    echo $namearray[$x];
+}
 
 // Itereer er over met een foreach-loop. Print ze af in HTML genummerde lijst.
 
