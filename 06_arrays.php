bc27b21eb2ee3254f0c9206a4ee1a7af7970d76b
Kathleen <kathleen.puvrez@immalle.be>
2017-02-22 19:06:37 +0100

    Oefening 6 af

--- 06_arrays.php
+++ 06_arrays.php
@@ -8,14 +8,23 @@ $namearray = ["Thom", "Bert", "Liam"];
 
 for ($x = 0; $x < $namearray.length; $x++){
     echo $namearray[$x];
+    echo "<br/>";
 }
 
 // Itereer er over met een foreach-loop. Print ze af in HTML genummerde lijst.
 
-
+foreach ( $namearray as &$value ) {
+    echo $value;
+    echo "<br/>";
+}
 
 $naam = "Willy";
 
 // Itereer met een for-loop over de variable $naam alsof het een array was.
+
+foreach ( $naam as &$value ) {
+    echo $value;
+    echo "<br/>";
+}
 
 ?>
